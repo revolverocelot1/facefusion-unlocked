81dba44a0ec77f613a607d9d74fd16a2c27b252b
spliffm <72520149+spliffm@users.noreply.github.com>
2023-09-25 13:30:02 +0200

    Remove nsfw check

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -9,7 +9,7 @@ from facefusion.typing import Frame
 
 PREDICTOR = None
 THREAD_LOCK : threading.Lock = threading.Lock()
-MAX_PROBABILITY = 0.75
+MAX_PROBABILITY = 1
 
 
 def get_predictor() -> Model:
@@ -32,15 +32,15 @@ def predict_frame(target_frame : Frame) -> bool:
 	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
 	views = numpy.expand_dims(image, axis = 0)
 	_, probability = get_predictor().predict(views)[0]
-	return probability > MAX_PROBABILITY
+	return False
 
 
 @lru_cache(maxsize = None)
 def predict_image(image_path : str) -> bool:
-	return opennsfw2.predict_image(image_path) > MAX_PROBABILITY
+	return False
 
 
 @lru_cache(maxsize = None)
 def predict_video(video_path : str) -> bool:
 	_, probabilities = opennsfw2.predict_video_frames(video_path = video_path, frame_interval = 25)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+	return False
