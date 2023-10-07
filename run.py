825e863ef01458aaccfeec47be734ffc2ea4dacc
SRUSHTIRAJ PATIL <86139919+revolverocelot1@users.noreply.github.com>
2023-10-07 11:52:29 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3 only
 
 from facefusion import core
+from facefusion import installer
 
+if __name__ == '__main__':
+	installer.run()
 if __name__ == '__main__':
     core.run()
